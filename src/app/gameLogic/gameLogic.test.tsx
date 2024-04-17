2c3c5eb488ab6981e89fa391d5066fcb6c277dcf
Katie Harrington <katie@sobski.com>
2024-04-17 14:49:16 -0500

    Add rules for solitaire process

--- src/app/gameLogic/gameLogic.test.tsx
+++ src/app/gameLogic/gameLogic.test.tsx
@@ -11,17 +11,17 @@ import GameLogic from './gameLogic';
 describe('GameLogic', () => {
   beforeEach(() => {
     const expectedDeck: Card[] = [
-      { value: 1, suit: Suit.Hearts }, // S
+      { value: 1, suit: Suit.Hearts }, // S  1
       { value: 1, suit: Suit.Spades }, // Q
-      { value: 1, suit: Suit.Spades }, // S
+      { value: 1, suit: Suit.Spades }, // S  2
       { value: 1, suit: Suit.Diamonds }, // J
-      { value: 1, suit: Suit.Diamonds }, // S
-      { value: 1, suit: Suit.Clubs }, // S
+      { value: 1, suit: Suit.Diamonds }, // S  3
+      { value: 1, suit: Suit.Clubs }, // S  4
       { value: 1, suit: Suit.Clubs }, // 10
 
       { value: 2, suit: Suit.Hearts }, // 9
       { value: 2, suit: Suit.Hearts }, // 8
-      { value: 2, suit: Suit.Spades }, // S
+      { value: 2, suit: Suit.Spades }, // S  5
       { value: 2, suit: Suit.Spades }, // 7
       { value: 2, suit: Suit.Diamonds }, // 6
       { value: 2, suit: Suit.Diamonds }, // 5
@@ -30,37 +30,37 @@ describe('GameLogic', () => {
 
       { value: 3, suit: Suit.Hearts }, // 2
       { value: 3, suit: Suit.Hearts }, // A
-      { value: 3, suit: Suit.Spades }, // S
+      { value: 3, suit: Suit.Spades }, // S  6
       { value: 3, suit: Suit.Spades }, // K
       { value: 3, suit: Suit.Diamonds }, // Q
       { value: 3, suit: Suit.Diamonds }, // J
-      { value: 3, suit: Suit.Clubs }, // S
+      { value: 3, suit: Suit.Clubs }, // S  7
       { value: 3, suit: Suit.Clubs }, // 10
 
       { value: 4, suit: Suit.Hearts }, // 9
       { value: 4, suit: Suit.Hearts }, // 8
-      { value: 4, suit: Suit.Spades }, // S
+      { value: 4, suit: Suit.Spades }, // S  8
       { value: 4, suit: Suit.Spades }, // 7
       { value: 4, suit: Suit.Diamonds }, // 6
       { value: 4, suit: Suit.Diamonds }, // 5
-      { value: 4, suit: Suit.Clubs }, // S
+      { value: 4, suit: Suit.Clubs }, // S  9
       { value: 4, suit: Suit.Clubs }, // 4
 
       { value: 5, suit: Suit.Hearts }, // 3
       { value: 5, suit: Suit.Hearts }, // 2
       { value: 5, suit: Suit.Spades }, // A
-      { value: 5, suit: Suit.Spades }, // S
+      { value: 5, suit: Suit.Spades }, // S  10
       { value: 5, suit: Suit.Diamonds }, // K
       { value: 5, suit: Suit.Diamonds }, // Q
       { value: 5, suit: Suit.Clubs }, // J
-      { value: 5, suit: Suit.Clubs }, // S
+      { value: 5, suit: Suit.Clubs }, // S  11
 
       { value: 6, suit: Suit.Hearts }, // 10
       { value: 6, suit: Suit.Hearts }, // 9
       { value: 6, suit: Suit.Spades }, // 8
-      { value: 6, suit: Suit.Spades }, // S
+      { value: 6, suit: Suit.Spades }, // S  12
       { value: 6, suit: Suit.Diamonds }, // 7
-      { value: 6, suit: Suit.Diamonds }, // S
+      { value: 6, suit: Suit.Diamonds }, // S  13
       { value: 6, suit: Suit.Clubs }, // 6
       { value: 6, suit: Suit.Clubs }, // 5
 
@@ -68,17 +68,17 @@ describe('GameLogic', () => {
       { value: 7, suit: Suit.Hearts }, // 3
       { value: 7, suit: Suit.Spades }, // 2
       { value: 7, suit: Suit.Spades }, // A
-      { value: 7, suit: Suit.Diamonds }, // S
+      { value: 7, suit: Suit.Diamonds }, // S  14
       { value: 7, suit: Suit.Diamonds }, // K
       { value: 7, suit: Suit.Clubs }, // Q
       { value: 7, suit: Suit.Clubs }, // J
 
-      { value: 8, suit: Suit.Hearts }, // S
+      { value: 8, suit: Suit.Hearts }, // S  15
       { value: 8, suit: Suit.Hearts }, // 10
       { value: 8, suit: Suit.Spades }, // 9
-      { value: 8, suit: Suit.Spades }, // S
+      { value: 8, suit: Suit.Spades }, // S  16
       { value: 8, suit: Suit.Diamonds }, // 8
-      { value: 8, suit: Suit.Diamonds }, // S
+      { value: 8, suit: Suit.Diamonds }, // S  17
       { value: 8, suit: Suit.Clubs }, // 7
       { value: 8, suit: Suit.Clubs }, // 6
 
@@ -87,7 +87,7 @@ describe('GameLogic', () => {
       { value: 9, suit: Suit.Spades }, // 3
       { value: 9, suit: Suit.Spades }, // 2
       { value: 9, suit: Suit.Diamonds }, // A
-      { value: 9, suit: Suit.Diamonds }, // S
+      { value: 9, suit: Suit.Diamonds }, // S  18
       { value: 9, suit: Suit.Clubs }, // K
       { value: 9, suit: Suit.Clubs }, // Q
 
@@ -351,6 +351,226 @@ describe('GameLogic', () => {
 
         expect(within(afterTableauPiles[12]).getByText('3')).toBeVisible();
         expect(within(afterTableauPiles[12]).getByAltText('of Diamonds')).toBeVisible();
+      });
+    });
+
+    describe('when an attempt is made to add a card to a foundation pile', () => {
+      it('ensures only an Ace can start the first 4 foundation piles', async () => {
+        const user = userEvent.setup();
+        render(<GameLogic />);
+
+        const tableauSection = screen.getByTestId('tableau');
+        const tableauPiles = within(tableauSection).getAllByRole('button');
+
+        await user.click(tableauPiles[0]);
+
+        const foundationSection = screen.getByTestId('foundations');
+        const foundations = within(foundationSection).getAllByRole('button');
+
+        expect(within(foundations[0]).getByText('A')).toBeVisible();
+        expect(within(foundations[0]).queryByText('3')).not.toBeInTheDocument();
+
+        await user.click(tableauPiles[12]);
+
+        const secondFoundationSection = screen.getByTestId('foundations');
+        const secondFoundations = within(secondFoundationSection).getAllByRole('button');
+
+        expect(within(secondFoundations[0]).getByText('A')).toBeVisible();
+        expect(within(secondFoundations[0]).getByAltText('of Spades')).toBeVisible();
+      });
+      it('ensures two Ace piles cannot share suits', async () => {
+        const user = userEvent.setup();
+        render(<GameLogic />);
+
+        const tableauSection = screen.getByTestId('tableau');
+        const tableauPiles = within(tableauSection).getAllByRole('button');
+
+        await user.click(tableauPiles[12]);
+        await user.click(tableauPiles[10]);
+        await user.click(within(screen.getByTestId('hand')).getAllByRole('button')[0]);
+        await user.click(tableauPiles[10]);
+        await user.click(within(screen.getByTestId('hand')).getAllByRole('button')[0]);
+
+        const foundationSection = screen.getByTestId('foundations');
+        const foundations = within(foundationSection).getAllByRole('button');
+
+        expect(within(foundations[0]).getByText('A')).toBeVisible();
+        expect(within(foundations[0]).getByAltText('of Spades')).toBeVisible();
+        expect(within(foundations[1]).getByText('A')).toBeVisible();
+        expect(within(foundations[1]).getByAltText('of Hearts')).toBeVisible();
+        expect(within(foundations[2]).getByText('A')).toBeVisible();
+        expect(within(foundations[2]).queryByAltText('of Spades')).not.toBeInTheDocument();
+      });
+      it('ensures that adding a card to an Ace pile requires an ascending card value', async () => {
+        const user = userEvent.setup();
+        render(<GameLogic />);
+
+        const tableauSection = screen.getByTestId('tableau');
+        const tableauPiles = within(tableauSection).getAllByRole('button');
+
+        await user.click(tableauPiles[12]);
+        await user.click(tableauPiles[13]);
+
+        const foundationSection = screen.getByTestId('foundations');
+        const foundations = within(foundationSection).getAllByRole('button');
+
+        expect(within(foundations[0]).getByText('A')).toBeVisible();
+        expect(within(foundations[0]).getByAltText('of Spades')).toBeVisible();
+
+        await user.click(tableauPiles[6]);
+        const secondFoundationSection = screen.getByTestId('foundations');
+        const secondFoundations = within(secondFoundationSection).getAllByRole('button');
+
+        expect(within(secondFoundations[0]).getByText('2')).toBeVisible();
+        expect(within(secondFoundations[0]).getByAltText('of Spades')).toBeVisible();
+      });
+      it('ensures only a King can start the last 4 foundation piles', async () => {
+        const user = userEvent.setup();
+        render(<GameLogic />);
+
+        const tableauSection = screen.getByTestId('tableau');
+        const tableauPiles = within(tableauSection).getAllByRole('button');
+
+        await user.click(tableauPiles[0]);
+
+        const foundationSection = screen.getByTestId('foundations');
+        const foundations = within(foundationSection).getAllByRole('button');
+
+        expect(within(foundations[5]).getByText('K')).toBeVisible();
+        expect(within(foundations[5]).queryByText('3')).not.toBeInTheDocument();
+
+        await user.click(tableauPiles[10]);
+        await user.click(within(screen.getByTestId('hand')).getAllByRole('button')[0]);
+        await user.click(tableauPiles[10]);
+        await user.click(within(screen.getByTestId('hand')).getAllByRole('button')[0]);
+        await user.click(tableauPiles[10]);
+        await user.click(within(screen.getByTestId('hand')).getAllByRole('button')[0]);
+        await user.click(tableauPiles[10]);
+        await user.click(within(screen.getByTestId('hand')).getAllByRole('button')[0]);
+        await user.click(tableauPiles[10]);
+        await user.click(tableauPiles[1]);
+        await user.click(within(screen.getByTestId('hand')).getByText('K'));
+
+        const secondFoundationSection = screen.getByTestId('foundations');
+        const secondFoundations = within(secondFoundationSection).getAllByRole('button');
+
+        expect(within(secondFoundations[4]).getByText('K')).toBeVisible();
+        expect(within(secondFoundations[4]).getByAltText('of Diamonds')).toBeVisible();
+      });
+      it('ensures two King piles cannot share suits', async () => {
+        const user = userEvent.setup();
+        render(<GameLogic />);
+
+        const tableauSection = screen.getByTestId('tableau');
+        const tableauPiles = within(tableauSection).getAllByRole('button');
+
+        await user.click(tableauPiles[10]);
+        await user.click(within(screen.getByTestId('hand')).getAllByRole('button')[0]);
+        await user.click(tableauPiles[10]);
+        await user.click(within(screen.getByTestId('hand')).getAllByRole('button')[0]);
+        await user.click(tableauPiles[10]);
+        await user.click(within(screen.getByTestId('hand')).getAllByRole('button')[0]);
+        await user.click(tableauPiles[10]);
+        await user.click(within(screen.getByTestId('hand')).getAllByRole('button')[0]);
+        await user.click(tableauPiles[10]);
+        await user.click(tableauPiles[1]);
+        await user.click(within(screen.getByTestId('hand')).getByText('K'));
+        await user.click(within(screen.getByTestId('hand')).getByText('2'));
+        await user.click(tableauPiles[1]);
+        await user.click(tableauPiles[10]);
+        await user.click(tableauPiles[2]);
+        await user.click(within(screen.getByTestId('hand')).getByText('K'));
+
+        const foundationSection = screen.getByTestId('foundations');
+        const foundations = within(foundationSection).getAllByRole('button');
+
+        expect(within(foundations[4]).getByText('K')).toBeVisible();
+        expect(within(foundations[4]).getByAltText('of Diamonds')).toBeVisible();
+        expect(within(foundations[5]).getByText('K')).toBeVisible();
+        expect(within(foundations[5]).queryByAltText('of Diamonds')).not.toBeInTheDocument();
+      });
+      it('ensures that adding a card to a King pile requires an descending card value', async () => {
+        const user = userEvent.setup();
+        render(<GameLogic />);
+
+        const tableauSection = screen.getByTestId('tableau');
+        const tableauPiles = within(tableauSection).getAllByRole('button');
+
+        //pull 18 cards to get a King and a Queen
+        await user.click(tableauPiles[10]); //1
+        await user.click(within(screen.getByTestId('hand')).getByText('A'));
+        await user.click(tableauPiles[10]); //2
+        await user.click(within(screen.getByTestId('hand')).getByText('A'));
+        await user.click(tableauPiles[10]); //3
+        await user.click(within(screen.getByTestId('hand')).getByText('A'));
+        await user.click(tableauPiles[10]); //4
+        await user.click(within(screen.getByTestId('hand')).getByText('A'));
+        await user.click(tableauPiles[10]); //5
+        await user.click(tableauPiles[1]);
+        await user.click(within(screen.getByTestId('hand')).getByText('K'));
+        await user.click(tableauPiles[1]);
+        await user.click(tableauPiles[10]); //6
+        await user.click(tableauPiles[2]);
+        await user.click(tableauPiles[2]);
+        await user.click(tableauPiles[10]); //7
+        await user.click(tableauPiles[2]);
+        await user.click(tableauPiles[2]);
+        await user.click(tableauPiles[10]); //8
+        await user.click(tableauPiles[3]);
+        await user.click(tableauPiles[3]);
+        await user.click(tableauPiles[10]); //9
+        await user.click(tableauPiles[3]);
+        await user.click(tableauPiles[3]);
+        await user.click(tableauPiles[10]); //10
+        await user.click(tableauPiles[4]);
+        await user.click(tableauPiles[4]);
+        await user.click(tableauPiles[10]); //11
+        await user.click(tableauPiles[4]);
+        await user.click(tableauPiles[4]);
+        await user.click(tableauPiles[10]); //12
+        await user.click(tableauPiles[5]);
+        await user.click(tableauPiles[5]);
+        await user.click(tableauPiles[10]); //13
+        await user.click(tableauPiles[5]);
+        await user.click(tableauPiles[5]);
+        await user.click(tableauPiles[10]); //14
+        await user.click(tableauPiles[6]);
+        await user.click(tableauPiles[6]);
+        await user.click(tableauPiles[10]); //15
+        await user.click(tableauPiles[7]);
+        await user.click(tableauPiles[7]);
+        await user.click(tableauPiles[10]); //16
+        await user.click(tableauPiles[7]);
+        await user.click(tableauPiles[7]);
+        await user.click(tableauPiles[10]); //17
+        await user.click(tableauPiles[7]);
+        await user.click(tableauPiles[7]);
+        await user.click(tableauPiles[10]); //18
+        await user.click(tableauPiles[8]);
+        await user.click(within(screen.getByTestId('hand')).getByText('Q'));
+        await user.click(within(screen.getByTestId('hand')).getByText('9'));
+
+        const foundationSection = screen.getByTestId('foundations');
+        const foundations = within(foundationSection).getAllByRole('button');
+
+        expect(within(foundations[4]).getByText('Q')).toBeVisible();
+        expect(within(foundations[4]).getByAltText('of Diamonds')).toBeVisible();
+      });
+      it('ensures that a card can only be added to a pile of the same suit', async () => {
+        const user = userEvent.setup();
+        render(<GameLogic />);
+
+        const tableauSection = screen.getByTestId('tableau');
+        const tableauPiles = within(tableauSection).getAllByRole('button');
+
+        await user.click(tableauPiles[12]);
+        await user.click(tableauPiles[8]);
+
+        const foundationSection = screen.getByTestId('foundations');
+        const foundations = within(foundationSection).getAllByRole('button');
+
+        expect(within(foundations[0]).getByText('A')).toBeVisible();
+        expect(within(foundations[0]).getByAltText('of Spades')).toBeVisible();
       });
     });
   });
