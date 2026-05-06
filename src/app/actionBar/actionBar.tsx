45bc21508eac6624b835c0ab15f59f4a3d0f0e39
Surfdaweb <katie@sobski.com>
2026-05-06 10:34:25 -0500

    Style action bar buttons

--- src/app/actionBar/actionBar.tsx
+++ src/app/actionBar/actionBar.tsx
@@ -9,13 +9,13 @@ export default function ActionBar({ undoMove, startNewGame }: ActionBarProps) {
   return (
     <>
       <div className={styles.actionBarContainer}>
-        <button className={styles.actionBarButton} onClick={undoMove}>
-          <img src="/Undo.png" alt="" />
-          Undo
-        </button>
         <button className={styles.actionBarButton} onClick={startNewGame}>
           <img src="/NewGame.png" alt="" />
           New Game
+        </button>
+        <button className={styles.actionBarButton} onClick={undoMove}>
+          <img src="/Undo.png" alt="" />
+          Undo
         </button>
       </div>
     </>
